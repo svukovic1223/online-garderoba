71d16fba82984b60ee325fbae679b4842984cb77
svukovic <svukovic1223it@raf.rs>
2025-12-22 14:14:22 +0100

    Konacna verzija Laravel aplikacije

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,6 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
         <div class="container">
-            <!-- <a class="navbar-brand" href="{{ url('/') }}">Online Shop</a> -->
             <div class="collapse navbar-collapse">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item"><a class="nav-link" href="{{ route('products.index') }}">Proizvodi</a></li>
